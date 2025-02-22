f1ecad62786fdde96a66778b8a9a415536b9efe8
Andrea Rosero <andrea.rosero.p@gmail.com>
2025-02-22 20:28:52 -0300

    capturando valor

--- app.js
+++ app.js
@@ -5,4 +5,9 @@ var amigos = []; // Crear un array para almacenar los nombres
 
 function agregarAmigo() {  // crear función que ingrese nombre y añada a la lista
 
+    const inputAmigo = document.getElementById("amigo"); //capturar el valor de entrada
+    const nombre = inputAmigo.value.trim(); //obtiene el valor del campo de entrada - trim elimina espacios en balnco al inicio y final
+    
+
+
 }
