176a50ab210a5e0150806a406b59b0cecac46305
Andrea Rosero <andrea.rosero.p@gmail.com>
2025-02-22 20:25:20 -0300

    array creado

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. 
 // Aquí deberás desarrollar la lógica para resolver el problema.
 
+var amigos = []; // Crear un array para almacenar los nombres
